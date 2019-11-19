22b7fbf5ecd075f830490d6b98d323483f4b6e06
KovalRomanK <romankoval10@gmail.com>
2019-11-19 18:19:23 -0500

    Fixed error

--- app/src/androidTest/java/mgarzon/createbest/productcatalog/FirstTest.java
+++ app/src/androidTest/java/mgarzon/createbest/productcatalog/FirstTest.java
@@ -4,6 +4,7 @@ package mgarzon.createbest.productcatalog;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -21,36 +22,36 @@ import static org.hamcrest.CoreMatchers.anything;
 
 
 @RunWith(AndroidJUnit4.class)
-    @LargeTest
-    public class FirstTest {
+@LargeTest
+public class FirstTest {
 
-        private String mStringToBetyped;
-        private String mNumberToBetyped;
-        @Rule
-        public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
-                MainActivity.class);
+    private String mStringToBetyped;
+    private String mNumberToBetyped;
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
 
-        @Before
-        public void initValidString() {
-            // Specify a valid string.
-            mStringToBetyped = "Espresso";
-            mNumberToBetyped = "1234";
-        }
-
-        @Test
-        public void changeText_sameActivity() {
-            // Type text and then press the button.
-            onView(withId(R.id.editTextName))
-                    .perform(typeText(mStringToBetyped), closeSoftKeyboard());
-            onView(withId(R.id.editTextPrice))
-                    .perform(typeText(mNumberToBetyped), closeSoftKeyboard());
-
-            onView(withId(R.id.addButton)).perform(click());
-
-            // Check that the text was changed.
-            //onView(withId(R.id.listViewProducts))
-              //      .check(matches(withText(mStringToBetyped)));
-            onData(anything()).inAdapterView(withId(R.id.listViewProducts)).atPosition(0).onChildView(withId(R.id.textViewName)).check(matches(withText("Hello")));
-
-        }
+    @Before
+    public void initValidString() {
+        // Specify a valid string.
+        mStringToBetyped = "Espresso";
+        mNumberToBetyped = "1234";
     }
+
+    @Test
+    public void changeText_sameActivity() {
+        // Type text and then press the button.
+        onView(withId(R.id.editTextName))
+                .perform(typeText(mStringToBetyped), closeSoftKeyboard());
+        onView(withId(R.id.editTextPrice))
+                .perform(typeText(mNumberToBetyped), closeSoftKeyboard());
+
+        onView(withId(R.id.addButton)).perform(click());
+
+        // Check that the text was changed.
+        onView(withId(R.id.listViewProducts))
+                .check(matches(withText(mStringToBetyped)));
+//            onData(anything()).inAdapterView(withId(R.id.listViewProducts)).atPosition(0).onChildView(withId(R.id.textViewName)).check(matches(withText("Hello")));
+
+    }
+}
